97ce1487613e9a0ea7711ce39c863db9fd578bdf
sebastiancodling <125040753+sebastiancodling@users.noreply.github.com>
2023-10-05 15:47:15 +0100

    Update goodevening.java

--- goodevening.java
+++ goodevening.java
@@ -1,5 +1,5 @@
 public class goodevening {
   public class void main(String[] arg){
-    System.out.println("Godon Ramsie is is a sussessful")
+    System.out.println("Godon Ramsie is is a sussessful storie of the fuud industrie")
       }
   }
