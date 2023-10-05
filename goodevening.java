95bab6b34267848886aa5ac3ee9d2daa93856f70
SEBASTIAN CODLING <s.codling.23@abdn.ac.uk>
2023-10-05 13:37:24 +0000

    Miss Leung talking

--- goodevening.java
+++ goodevening.java
@@ -1,5 +1,5 @@
 public class goodevening {
   public class void main(String[] arg){
-    System.out.println("Good evening, I'm Miss Leung, heads of foos science")
+    System.out.println("Good evening, I'm Miss Leung, heads of foos science. In this cos, your chils will learn about nutrishuns")
       }
   }
