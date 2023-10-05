ab8809a74c91612c3bfcc41ca5a3bf3537394dd9
sebastiancodling <125040753+sebastiancodling@users.noreply.github.com>
2023-10-05 15:55:11 +0100

    Update goodevening.java

--- goodevening.java
+++ goodevening.java
@@ -1,5 +1,5 @@
 public class goodevening {
   public class void main(String[] arg){
-    System.out.println("Godon Ramsie is is a sussessful storie of the fuud industrie")
+    System.out.println("is is just like Master Chef")
       }
   }
